f57f7b1346d9a1d65fc617801a3782c244b02be7
Realsammy <realsammy86@gmail.com>
2022-08-09 16:07:48 +0100

    Adding Project

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 import { urlFor } from '../lib/client'
 
 
-const HeroBanner = ({ heroBanner: {midText,image,buttonText,largeText1,smallText,product} }) => {
+const HeroBanner = ({ heroBanner: {midText,image,buttonText,largeText1,smallText, product} }) => {
   return (
     <div className='hero-banner-container'>
         <div>
             <p className='beat-solo'>{smallText}</p>
             <h3>{midText}</h3>
             <h2>{largeText1}</h2>
-            <img src= {urlFor(image)} alt= "headphone"
-             className='hero-banner-image' />
+            <img src= {urlFor(image)} alt= "headphone" 
+          className='hero-banner-image' />
 
         </div>
         <Link href={'/product/${heroBanner.product}'}>
@@ -21,7 +21,7 @@ const HeroBanner = ({ heroBanner: {midText,image,buttonText,largeText1,smallText
     
         <div className='desc'>
             <h5>DESCRIPTION</h5>
-            
+            <p>{product}</p>
 
         </div>
     </div>
